a8b271c1075fd5758bf2ac28528883d82d7c4c8f
Admin <Admin@DESKTOP-8A46KLF>
2021-03-15 13:05:53 +0530

    SDET_10 FW

---
+++ com.maven/src/main/java/com/maven/contacttest/ContactTest.java
@@ -0,0 +1,17 @@
+package com.maven.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontact() {
+		System.out.println("execute create contact");
+		}
+	public void modifycontacttest() {
+		System.out.println("execute modifycontacttest");
+		}
+	public void deletecontacttest() {
+		System.out.println("execute deletecontacttest");
+		}
+
+}
